7e0dbc9bf8759a08a990b0262e788068c39e4f27
imrekb <imrekbg@gmail.com>
2018-07-13 08:58:16 +0300

    feat: added flexboxgrid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -12,6 +13,7 @@ import '../polyfills';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { WebviewDirective } from './directives/webview.directive';
 import { ElectronService } from './providers/electron.service';
 import { ConversionComponent } from './routes/conversion/conversion.component';
@@ -26,13 +28,15 @@ export function HttpLoaderFactory(http: HttpClient) {
     declarations: [
         AppComponent,
         ConversionComponent,
-        WebviewDirective
+        WebviewDirective,
+        NavigationComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpClientModule,
         AppRoutingModule,
+        FlexLayoutModule,
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
